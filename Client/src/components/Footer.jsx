e0d117d6ff6c42edbce575abcd366b20f449a97e
adrian <adr.wijayaa@gmail.com>
2025-11-27 14:25:02 +0700

    revert

--- Client/src/components/Footer.jsx
+++ Client/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Footer.css";
-import logo1 from "../assets/img/BookingLiveaboard2.png";
+import logo1 from "../assets/img/BookingLiveaboard5.png";
 
 const Footer = ({ onBookNowClick, page }) => {
   const handleWhatsAppClick = (e) => {
